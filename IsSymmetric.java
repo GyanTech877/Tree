4729942481377fcd0c3a77615b6a9eb952fc1717
Tech Gyan <53473704+GyanTech877@users.noreply.github.com>
2019-11-18 10:27:12 +0530

    Update IsSymmetric.java

--- IsSymmetric.java
+++ IsSymmetric.java
@@ -16,3 +16,28 @@ public class Solution1 {
         return (root1.val == root2.val) && isSymmetricRec(root1.left,root2.right) && isSymmetricRec(root1.right,root2.left); 
     }
 }
+
+public class Solution2 {
+    /**
+     * @param root: root of the given tree
+     * @return: whether it is a mirror of itself 
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while(!queue.isEmpty()){
+            TreeNode leftNode = queue.poll();
+            TreeNode rightNode = queue.poll();
+            if(leftNode == null && rightNode ==null) return true;
+            if(leftNode == null || rightNode ==null) return false;
+            if(leftNode.val != rightNode.val) return false;
+            queue.offer(leftNode.left);
+            queue.offer(rightNode.right);
+            queue.offer(leftNode.right);
+            queue.offer(rightNode.left);
+        }
+        return true;
+    }
+}
